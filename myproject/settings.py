195e6f8298fccc54c5796b3063695c1ca8fbf770
Muhammad Sadman <sadmanbinsaleh543@gmail.com>
2026-06-09 19:58:49 +0600

    fix: add STATIC_ROOT for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,6 +143,4 @@ STATIC_URL = "static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
